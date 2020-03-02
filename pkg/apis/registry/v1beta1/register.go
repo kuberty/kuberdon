f346423444b08ee76dc05f75bbb5fbb2450b5edb
toonsevrin <twan123@live.be>
2020-03-02 22:03:10 +0100

    Updated scheme builder to fix bug

--- pkg/apis/registry/v1beta1/register.go
+++ pkg/apis/registry/v1beta1/register.go
@@ -15,11 +15,16 @@ var SchemeGroupVersion = schema.GroupVersion{
 	Version: "v1beta1",
 }
 
-var AddToScheme = runtime.NewSchemeBuilder(addKnownTypes).AddToScheme
-
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
+
+var (
+	// SchemeBuilder initializes a scheme builder
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	// AddToScheme is a global function that registers this API group & version to a scheme
+	AddToScheme = SchemeBuilder.AddToScheme
+)
 
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(
